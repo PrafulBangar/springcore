adce9262ab901767cc658e641e6db74061f00d85
Praful Bangar <52483773+PrafulBangar@users.noreply.github.com>
2021-10-07 12:39:26 +0530

    collections set map list added using setter injection

---
+++ src/main/java/com/springcore/collections/TestCollections.java
@@ -0,0 +1,23 @@
+package com.springcore.collections;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestCollections {
+
+	public static void main(String[] args) {
+
+		
+		ApplicationContext applicationContext=new  ClassPathXmlApplicationContext("com/springcore/collections/collectionconfig.xml");
+		
+		Emp emp1Objcet=(Emp) applicationContext.getBean("emp1");
+		
+		System.out.println(emp1Objcet.getEmployeeName());
+		System.out.println(emp1Objcet.getAddresses());
+		System.out.println(emp1Objcet.getPhones());
+		System.out.println(emp1Objcet.getCourses());
+		
+		
+	}
+
+}
